8812290e161b287ea86f035c258b92d558ba31e0
Sergio Sánchez <sss4esob@gmail.com>
2025-02-22 15:33:26 +0100

    Implement several improvements

--- app/src/main/java/com/dreamsoftware/lingosnap/ui/components/CameraPreview.kt
+++ app/src/main/java/com/dreamsoftware/lingosnap/ui/components/CameraPreview.kt
@@ -4,7 +4,7 @@ import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
 
 /**
